f25e11f4abcfdae0f01363d2302d2e4e93a858c4
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-01-25 19:40:29 -0300

    Autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController as UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::apiResource('users', UserController::class);
+
+    });
+
 Route::apiResource('products', ProductController::class);
+
+Route::post("login",[UserController::class,'index']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
